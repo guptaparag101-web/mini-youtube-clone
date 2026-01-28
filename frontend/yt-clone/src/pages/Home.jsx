4719054c1782b3667ad308d2c8bdf2a7310b3259
guptaparag101-web <guptaparag101@gmail.com>
2026-01-28 22:18:01 +0530

    fix case-sensitive VideoCard component name

--- frontend/yt-clone/src/pages/Home.jsx
+++ frontend/yt-clone/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import VideoCard from "../components/VideoCard";
+import VideoCard from "../components/temp";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
