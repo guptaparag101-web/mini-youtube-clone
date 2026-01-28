9fd7595c063ea5780f1ee7648b586dc2f3527762
guptaparag101-web <guptaparag101@gmail.com>
2026-01-28 21:52:32 +0530

    fix VideoCard filename case

--- frontend/yt-clone/src/pages/Home.jsx
+++ frontend/yt-clone/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import VideoCard from "../components/videocard";
+import VideoCard from "../components/VideoCard";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
