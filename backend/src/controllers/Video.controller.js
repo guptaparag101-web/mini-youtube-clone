697dd0ef4a187c82cac0b453eb4f3a3d47a10cb6
guptaparag101-web <guptaparag101@gmail.com>
2026-01-29 11:29:25 +0530

    fix undefined video controller exports

--- backend/src/controllers/Video.controller.js
+++ backend/src/controllers/Video.controller.js
@@ -1,4 +1,3 @@
-const cloudinary = require("../config/cloudinary");
 const Video = require("../models/Video.model");
 
 exports.uploadVideo = async (req, res) => {
@@ -7,29 +6,11 @@ exports.uploadVideo = async (req, res) => {
       return res.status(400).json({ message: "Files missing" });
     }
 
-    // upload video
-    const videoUpload = await cloudinary.uploader.upload(
-      `data:video/mp4;base64,${req.files.video[0].buffer.toString("base64")}`,
-      {
-        resource_type: "video",
-        folder: "yt-clone/videos",
-      }
-    );
-
-    // upload thumbnail
-    const thumbUpload = await cloudinary.uploader.upload(
-      `data:image/png;base64,${req.files.thumbnail[0].buffer.toString("base64")}`,
-      {
-        resource_type: "image",
-        folder: "yt-clone/thumbnails",
-      }
-    );
-
     const video = await Video.create({
       title: req.body.title,
       description: req.body.description,
-      videoUrl: videoUpload.secure_url,
-      thumbnailUrl: thumbUpload.secure_url,
+      videoUrl: req.files.video[0].path,
+      thumbnailUrl: req.files.thumbnail[0].path,
       channelName: req.user.channelName,
       user: req.user.id,
     });
@@ -39,4 +20,14 @@ exports.uploadVideo = async (req, res) => {
     console.error("UPLOAD ERROR:", err);
     res.status(500).json({ message: err.message });
   }
+};
+
+exports.getAllVideos = async (req, res) => {
+  const videos = await Video.find().sort({ createdAt: -1 });
+  res.json(videos);
+};
+
+exports.getSingleVideo = async (req, res) => {
+  const video = await Video.findById(req.params.id);
+  res.json(video);
 };
